36be89a4362ed8fcc7b1e187c9d6ba16f0edd56c
Ian Kim <iank0426@gmail.com>
2026-06-12 18:44:22 -0400

    trying to get git pages to work

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: "iankim0.github.io",
   plugins: [react(), tailwindcss()],
 });
